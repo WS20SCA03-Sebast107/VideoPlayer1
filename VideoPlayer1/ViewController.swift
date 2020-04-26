8e9e0635139f4b5f6fcebe61d1fddb4fde5cbb0a
sebastianDerosa <sebast710@gmail.com>
2020-04-26 17:46:59 -0400

    Video Player App
    Video Player

--- VideoPlayer1/ViewController.swift
+++ VideoPlayer1/ViewController.swift
@@ -6,13 +6,36 @@
 //  Copyright © 2020 BMCC. All rights reserved.
 //
 
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Sebastian Derosa on 4/26/20.
+//  Copyright © 2020 BMCC. All rights reserved.
+//
+
+import AVKit
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "Bird", ofType: "mp4")!))
+        player.volume = 30;
+        _ = AVPlayerLayer(player: player)
+        
+
+        let vc = AVPlayerViewController();
+        vc.player = player
+        present(vc, animated: true)
     }
 
 
