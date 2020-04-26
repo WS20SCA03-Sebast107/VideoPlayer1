3b5d30df1f5628db125663a4b585fb65cc15b87e
sebastianDerosa <sebast710@gmail.com>
2020-04-26 17:36:31 -0400

    Initial Commit

---
+++ VideoPlayer1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer1
+//
+//  Created by Sebastian Derosa on 4/26/20.
+//  Copyright © 2020 BMCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
